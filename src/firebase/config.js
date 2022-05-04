3f4aab796be025f2e3719bcd8e2bf10ad61e9f5f
Narendra Kumar <104636206+narendra28051991@users.noreply.github.com>
2022-05-04 21:57:54 +0200

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkPtlKJasNtvOXdvC4xKzRRbULlMZezNQ",
+  authDomain: "automatic-memory-game.firebaseapp.com",
+  projectId: "automatic-memory-game",
+  storageBucket: "automatic-memory-game.appspot.com",
+  messagingSenderId: "697890310562",
+  appId: "1:697890310562:web:ae08ae43124bebd8400afb"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
